6e6f4b5f2e1dc01d7f0ac2aa2c2229cd849cedbc
Alexande B <abobrikovich@gmail.com>
2022-06-22 15:02:18 +0200

    Add getSysDebug to HCaptchaDebugInfo

--- sdk/src/main/java/com/hcaptcha/sdk/HCaptchaDebugInfo.java
+++ sdk/src/main/java/com/hcaptcha/sdk/HCaptchaDebugInfo.java
@@ -7,79 +7,140 @@ import android.webkit.JavascriptInterface;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dalvik.system.DexFile;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Serializable;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 /**
  * Debug info for CI.
  */
 @Data
-@RequiredArgsConstructor
-class HCaptchaDebugInfo implements Serializable {
-
+@AllArgsConstructor
+final class HCaptchaDebugInfo implements Serializable {
     public static final String JS_INTERFACE_TAG = "JSDI";
+    private static final String GET_PROP_BIN = "/system/bin/getprop";
 
-    private static String sCache;
+    private static String sDebugInfo;
+    private static String sSysDebug;
 
     @NonNull
     private final Context context;
 
     @JavascriptInterface
     public String getDebugInfo() {
-        if (sCache != null) {
-            return sCache;
+        if (sDebugInfo != null) {
+            return sDebugInfo;
         }
         synchronized (this) {
-            if (sCache != null) {
-                return sCache;
+            if (sDebugInfo != null) {
+                return sDebugInfo;
             }
 
             try {
                 List<String> debugInfo = debugInfo(context.getPackageName(), context.getPackageCodePath());
                 final ObjectMapper objectMapper = new ObjectMapper();
-                sCache = objectMapper.writeValueAsString(debugInfo);
-            } catch (IOException | NoSuchAlgorithmException | CloneNotSupportedException e) {
+                sDebugInfo = objectMapper.writeValueAsString(debugInfo);
+            } catch (IOException | NoSuchAlgorithmException e) {
                 Log.d(JS_INTERFACE_TAG, "Cannot build debugInfo");
                 return "[]";
             }
-            return sCache;
+            return sDebugInfo;
+        }
+    }
+
+    @JavascriptInterface
+    public String getSysDebug() {
+        if (sSysDebug != null) {
+            return sSysDebug;
+        }
+        synchronized (this) {
+            if (sSysDebug != null) {
+                return sSysDebug;
+            }
+
+            try {
+                Map<String, String> props = roBuildProps();
+                final ObjectMapper objectMapper = new ObjectMapper();
+                sSysDebug = objectMapper.writeValueAsString(props);
+            } catch (IOException e) {
+                Log.d(JS_INTERFACE_TAG, "Cannot build sysDebug");
+                return "{}";
+            }
+            return sSysDebug;
         }
     }
 
     private List<String> debugInfo(String packageName, String packageCode)
-            throws IOException, NoSuchAlgorithmException, CloneNotSupportedException {
+            throws IOException, NoSuchAlgorithmException {
         List<String> result = new ArrayList<>(512);
         DexFile dexFile = new DexFile(packageCode);
         Enumeration<String> classes = dexFile.entries();
         MessageDigest androidMd5 = MessageDigest.getInstance("MD5");
         MessageDigest appMd5 = MessageDigest.getInstance("MD5");
         MessageDigest depsMd5 = MessageDigest.getInstance("MD5");
+        final String charsetName = "UTF-8";
         while (classes.hasMoreElements()) {
             String cls = classes.nextElement();
             if (cls.startsWith("com.google.android.") || cls.startsWith("android.")) {
-                androidMd5.update(cls.getBytes("UTF-8"));
+                androidMd5.update(cls.getBytes(charsetName));
             } else if (cls.startsWith(packageName)) {
-                appMd5.update(cls.getBytes("UTF-8"));
+                appMd5.update(cls.getBytes(charsetName));
             } else {
-                depsMd5.update(cls.getBytes("UTF-8"));
+                depsMd5.update(cls.getBytes(charsetName));
             }
         }
-        result.add("sys_" + String.format("%032x", new BigInteger(1, androidMd5.digest())));
-        result.add("deps_" + String.format("%032x", new BigInteger(1, depsMd5.digest())));
-        result.add("app_" + String.format("%032x", new BigInteger(1, appMd5.digest())));
+        final String hexFormat = "%032x";
+        result.add("sys_" + String.format(hexFormat, new BigInteger(1, androidMd5.digest())));
+        result.add("deps_" + String.format(hexFormat, new BigInteger(1, depsMd5.digest())));
+        result.add("app_" + String.format(hexFormat, new BigInteger(1, appMd5.digest())));
         result.add("aver_" + Build.VERSION.RELEASE);
         result.add("sdk_" + BuildConfig.VERSION_NAME.replace('.', '_'));
         return result;
+    }
+
+    private Map<String, String> roBuildProps() throws IOException {
+        Process getpropProcess = null;
+        Map<String, String> props = new HashMap<>();
+        try {
+            getpropProcess = new ProcessBuilder(GET_PROP_BIN).start();
+            try (BufferedReader br = new BufferedReader(
+                    new InputStreamReader(getpropProcess.getInputStream(), "UTF-8"))) {
+                String line;
+                StringBuilder entry = new StringBuilder();
+                while ((line = br.readLine()) != null) {
+                    if (line.endsWith("]")) {
+                        entry.replace(0, entry.length() == 0 ? 0 : entry.length() - 1, line);
+                    } else {
+                        entry.append(line);
+                        continue;
+                    }
+                    String[] parsedLine = entry.toString().split("]: \\[");
+                    String key = parsedLine[0].substring(1); // strip first [
+                    if (key.startsWith("ro")) {
+                        String value = parsedLine[1].substring(0, parsedLine[1].length() - 2); // strip last ]
+                        props.put(key, value);
+                    }
+                }
+            }
+        } finally {
+            if (getpropProcess != null) {
+                getpropProcess.destroy();
+            }
+        }
+        return props;
     }
 }
