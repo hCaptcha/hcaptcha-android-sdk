9f32ce4b4cbaba319f4c4faf07b993cea20ca6f9
Alex Babrykovich <abobrikovich@gmail.com>
2023-01-23 13:31:42 +0100

    refactor: generate html from java (#80)
    * #62 generate HCaptchaHtml.java from hcaptcha.html
    
    * Don't keep generated HCaptchaHtml.java in git
    
    * Fix sonar issues for hcaptcha.html
    
    * fix: not centered
    
    * Add missing DOCTYPE HTML to fix codesmell
    
    * Do clean before building aar from default branch
    
    * feat(sdk): move android.permission.INTERNET to sdk's AndroidManifest
    
    * chore(test): add e2e tests with debug token
    
    * test(sdk): increase e2e test timeouts
    
    Co-authored-by: Sergiu Danalachi <danalachi.sergiu@gmail.com>

--- sdk/src/androidTest/java/com/hcaptcha/sdk/HCaptchaTest.java
+++ sdk/src/androidTest/java/com/hcaptcha/sdk/HCaptchaTest.java
@@ -2,6 +2,7 @@ package com.hcaptcha.sdk;
 
 import static com.hcaptcha.sdk.AssertUtil.waitHCaptchaWebViewToken;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import androidx.test.core.app.ActivityScenario;
@@ -13,9 +14,11 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class HCaptchaTest {
     private static final long AWAIT_CALLBACK_MS = 5000;
+    private static final long E2E_AWAIT_CALLBACK_MS = AWAIT_CALLBACK_MS * 2;
 
     @Rule
     public ActivityScenarioRule<TestActivity> rule = new ActivityScenarioRule<>(TestActivity.class);
@@ -80,5 +83,57 @@ public class HCaptchaTest {
         });
 
         waitHCaptchaWebViewToken(latch, AWAIT_CALLBACK_MS);
+    }
+
+    @Test
+    public void e2eWithDebugTokenFragmentDialog() throws Exception {
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        final ActivityScenario<TestActivity> scenario = rule.getScenario();
+        scenario.onActivity(activity -> {
+            HCaptcha.getClient(activity)
+                    .verifyWithHCaptcha(config.toBuilder().hideDialog(false).build())
+                    .addOnSuccessListener(new OnSuccessListener<HCaptchaTokenResponse>() {
+                        @Override
+                        public void onSuccess(HCaptchaTokenResponse response) {
+                            response.markUsed();
+                            latch.countDown();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(HCaptchaException exception) {
+                            fail("No errors expected");
+                        }
+                    });
+        });
+
+        assertTrue(latch.await(E2E_AWAIT_CALLBACK_MS, TimeUnit.MILLISECONDS));
+    }
+
+    @Test
+    public void e2eWithDebugTokenHeadlessWebView() throws Exception {
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        final ActivityScenario<TestActivity> scenario = rule.getScenario();
+        scenario.onActivity(activity -> {
+            HCaptcha.getClient(activity)
+                    .verifyWithHCaptcha(config.toBuilder().hideDialog(true).build())
+                    .addOnSuccessListener(new OnSuccessListener<HCaptchaTokenResponse>() {
+                        @Override
+                        public void onSuccess(HCaptchaTokenResponse response) {
+                            response.markUsed();
+                            latch.countDown();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(HCaptchaException exception) {
+                            fail("No errors expected");
+                        }
+                    });
+        });
+
+        assertTrue(latch.await(E2E_AWAIT_CALLBACK_MS, TimeUnit.MILLISECONDS));
     }
 }
